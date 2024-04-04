83380737758ca23b55b2162d706afe352c027e64
syumai <syumai@gmail.com>
2024-04-05 02:43:56 +0900

    add Combox implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-// TODO
+import { comlinkCode } from "./lib/comlink@4.4.1.min";
+import { Remote, releaseProxy, wrap } from "comlink";
+
+const workerCode = `
+${comlinkCode};
+Comlink.expose({
+  evaluate(code) {
+    const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+    return (new AsyncFunction('"use strict"; return (' + code + ');'))();
+  },
+});
+delete globalThis.Comlink;
+`;
+const workerBlob = new Blob([workerCode], { type: "text/javascript" });
+const workerUrl = URL.createObjectURL(workerBlob);
+
+type ComboxWorker = {
+  evaluate: (code: string) => Promise<any>;
+};
+
+export class Combox {
+  #worker: Worker;
+  #comboxWorker: Remote<ComboxWorker>;
+  constructor() {
+    this.#worker = new Worker(workerUrl);
+    this.#comboxWorker = wrap<ComboxWorker>(this.#worker);
+  }
+  async evaluate(code: string): Promise<any> {
+    return this.#comboxWorker.evaluate(code);
+  }
+  release() {
+    this.#comboxWorker[releaseProxy]();
+    this.#worker.terminate();
+  }
+}
